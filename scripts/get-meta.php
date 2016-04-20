8e587512b23144022d3dedec8c8b0f6b3d9e6734
Samuli <samuli@fenidata.fi>
2016-04-20 18:00:59 +0300

    GET META
    NEED FOR SPEED

---
+++ scripts/get-meta.php
@@ -0,0 +1,39 @@
+function file_get_contents_curl($url)
+{
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+}
+
+$html = file_get_contents_curl("http://example.com/");
+
+//parsing begins here:
+$doc = new DOMDocument();
+@$doc->loadHTML($html);
+$nodes = $doc->getElementsByTagName('title');
+
+//get and display what you need:
+$title = $nodes->item(0)->nodeValue;
+
+$metas = $doc->getElementsByTagName('meta');
+
+for ($i = 0; $i < $metas->length; $i++)
+{
+    $meta = $metas->item($i);
+    if($meta->getAttribute('name') == 'description')
+        $description = $meta->getAttribute('content');
+    if($meta->getAttribute('name') == 'keywords')
+        $keywords = $meta->getAttribute('content');
+}
+
+echo "Title: $title". '<br/><br/>';
+echo "Description: $description". '<br/><br/>';
+echo "Keywords: $keywords";
